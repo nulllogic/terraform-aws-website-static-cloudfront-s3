f523af92d84e9c5ce7015b346b210d9a06d0e486
Vladimir Lukyanov <vladimir@lukyanov.net>
2023-12-26 21:39:45 -1000

    v0.1.0
    - s3+cloudfront is working great

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "bucket_name" {
-  value = aws_s3_bucket.main.bucket
+  description = "Bucket name corresponding to the S3."
+  value       = aws_s3_bucket.main.bucket
 }
 
 output "cloudfront_url" {
-  value = aws_cloudfront_distribution.cloudfront.domain_name
+  description = "Domain name corresponding to the CloudFront distribution."
+  value       = aws_cloudfront_distribution.cloudfront.cloudfront_url
 }
