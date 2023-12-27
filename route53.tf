8f9e35ef1abbfa65c4efb5b171a21344ad732650
Vladimir Lukyanov <vladimir@lukyanov.net>
2023-12-26 19:28:07 -1000

    v0.0.992

--- route53.tf
+++ route53.tf
@@ -9,26 +9,6 @@ data "aws_route53_zone" "zone" {
   private_zone = false
 }
 
-resource "aws_route53_record" "cert_validation" {
-  count    = !try(var.route53.domain, null) ? 1 : 0
-  provider = aws.main
-
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.zone.zone_id
-  ttl             = 60
-}
-
 resource "aws_route53_record" "website" {
   count = !try(var.route53.domain, null) ? 1 : 0
 
