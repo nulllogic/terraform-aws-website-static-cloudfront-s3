50fdbbf7e766181a3d44717be7b6a48fcb137302
Vladimir Lukyanov <vladimir@lukyanov.net>
2023-12-26 20:28:50 -1000

    v0.0.993

--- acm.tf
+++ acm.tf
@@ -15,6 +15,32 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+data "aws_route53_zone" "zone" {
+  count        = !try(var.route53.domain, null) ? 1 : 0
+  provider     = aws.main
+  name         = var.route53.domain
+  private_zone = false
+}
+
+resource "aws_route53_record" "cert_validation" {
+  provider = aws.main
+
+  for_each = !try(var.route53.domain, null) ? {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  } : {}
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
+  zone_id         = !try(var.route53.domain, null) ? data.aws_route53_zone.zone.zone_id : "0123456789ABCDEFGHIJK"
+  ttl             = 60
+}
+
 resource "aws_acm_certificate_validation" "cert" {
   count    = !try(var.route53.domain, null) ? 1 : 0
   provider = aws.acm_provider
