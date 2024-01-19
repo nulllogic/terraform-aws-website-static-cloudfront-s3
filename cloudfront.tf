8fc0503c108cbb76b5c72641e3aed31ca142ba35
Vladimir Lukyanov <vladimir@lukyanov.net>
2024-01-19 21:03:39 +0000

    v0.4.1
    v0.4.1

--- cloudfront.tf
+++ cloudfront.tf
@@ -158,7 +158,7 @@ resource "aws_cloudfront_response_headers_policy" "security" {
       override = true
     }
     content_security_policy {
-      content_security_policy = "frame-ancestors 'none'; default-src 'none'; img-src 'self'; script-src 'self'; style-src 'selt'; object-src 'none'"
+      content_security_policy = "frame-ancestors 'self'; default-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'self'"
       override = true
     }
   }
