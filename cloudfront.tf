478a3146550277f489bc658633fd60d533402ec9
Vladimir Lukyanov <vladimir@lukyanov.net>
2024-01-20 01:14:04 +0000

    v0.5.0
    v0.5.0

--- cloudfront.tf
+++ cloudfront.tf
@@ -135,28 +135,34 @@ resource "aws_cloudfront_response_headers_policy" "security" {
   }
   
   security_headers_config {
+    
     content_type_options {
       override = true
     }
+
     frame_options {
       frame_option = "DENY"
       override = true
     }
+
     referrer_policy {
       referrer_policy = "same-origin"
       override = true
     }
+
     xss_protection {
       mode_block = true
       protection = true
       override = true
     }
+
     strict_transport_security {
       access_control_max_age_sec = "63072000"
       include_subdomains = true
       preload = true
       override = true
     }
+
     content_security_policy {
       content_security_policy = replace(trimspace(<<EOT
 frame-ancestors 'self';
