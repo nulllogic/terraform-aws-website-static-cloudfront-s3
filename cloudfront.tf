9e6311a8fd2570668f021394b1c040dcb41e8d59
Vladimir Lukyanov <vladimir@lukyanov.net>
2025-08-05 21:33:06 +0300

    v0.5.76 – cloudfront terraform update

--- cloudfront.tf
+++ cloudfront.tf
@@ -233,7 +233,7 @@ resource "aws_cloudfront_response_headers_policy" "security" {
     }
 
     content_security_policy {
-      content_security_policy = "frame-ancestors 'none'; default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+      content_security_policy = "frame-ancestors 'none'; default-src 'none'; img-src *; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; object-src 'none'"
       override = true
     }
 
